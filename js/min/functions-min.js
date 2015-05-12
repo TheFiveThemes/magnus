17667d3c16687274bb96e9e4f4c89aa173311944
Hugo Baeta <hugo@baeta.me>
2015-05-11 22:50:07 -0700

    Remove unnecessary files

--- js/min/functions-min.js
+++
@@ -1,1 +0,0 @@
-function bigImageClass(){$(".entry-content img.size-full").each(function(){var a=$(this),t=$(this).closest("figure"),s=new Image;s.src=a.attr("src"),$(s).load(function(){var e=s.width;e>=1088&&$(a).addClass("size-big"),t.hasClass("wp-caption")&&e>=1088&&(t.addClass("caption-big"),t.removeAttr("style"))})})}$(document).ready(function(){$("#fullpage").fullpage()});
