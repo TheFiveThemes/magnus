56d33fb05f867e5adb9e3810b6892a86307609a2
Hugo Baeta <hugo@baeta.me>
2016-02-20 20:24:30 -0800

    removed wrong text domain placed in by mistake

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<ul class="site-info">
       <li class="cms-info"><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'magnus' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'magnus' ), 'WordPress' ); ?></a></li>
-			<li class="theme-info"><a href="http://thefivethemes.com"><?php printf( __( 'Theme: %1$s by %2$s for %3$s', 'invenio' ), 'Magnus', 'Hugo&nbsp;Baeta', 'The&nbsp;Five&nbsp;Themes' ); ?></a></li>
+			<li class="theme-info"><a href="http://thefivethemes.com"><?php printf( __( 'Theme: %1$s by %2$s for %3$s', 'magnus' ), 'Magnus', 'Hugo&nbsp;Baeta', 'The&nbsp;Five&nbsp;Themes' ); ?></a></li>
 		</ul><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
