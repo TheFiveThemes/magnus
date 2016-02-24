68400ed963fb40187ca411006336504654c4394b
Hugo Baeta <hugo@baeta.me>
2016-02-24 13:16:18 -0800

    Fixed #26

--- archive.php
+++ archive.php
@@ -16,8 +16,8 @@ get_header(); ?>
 
 			<header class="page-header">
 				<?php
-					magnus_archive_title( '<h1 class="page-title">', '</h1>' );
-					magnus_archive_description( '<div class="page-description">', '</div>' );
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="page-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
